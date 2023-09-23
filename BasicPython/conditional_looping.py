aeefdf60271874a94677e24e5437c64076dcbd66
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2023-09-23 16:35:24 +0530

    Add files via upload

---
+++ BasicPython/conditional_looping.py
@@ -0,0 +1,25 @@
+# a = int(input("Enter the value of a : "))
+# if a > 8 :
+#     print("a is greater than 8")
+# elif a > 5:
+#     print("a is greater than 5 as well as less than 8")
+# else:
+#     print("a is greater than 8")
+
+
+# a = True
+# if a is True:
+#     print("a is True")
+
+# a = "hello, dear how was your code gone."
+# if "hello" in a:
+#     print("hello is in a")
+
+
+# for i in range(0,11,1):
+#     print("*"*i)
+#     if i ==9:
+#         break
+# else:
+#     print("Loop is completed successfully")
+
