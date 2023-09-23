be4bb94cbb26298e9ae9b086b95f85e3295eb34e
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2023-09-24 01:08:52 +0530

    Add files via upload

---
+++ ProblemsSolution/Palindrom.py
@@ -0,0 +1,9 @@
+# Check the string is palindrom or not
+text = input("Enter the text for checking it is palindrom or not : ")
+
+# Approach 1
+# text1 = text[ : : -1]
+# if text1 == text:
+#     print(f"{text} is a palindrome, because reverse of it is {text1}")
+# else:
+#     print(f"{text} is not a palindrome, because reverse of it is {text1}")
---
+++ ProblemsSolution/presenceOfWord.py
@@ -0,0 +1,8 @@
+# Find the sub string presennce in the string or not
+# text = "hello dear, I am here, I hope you are well and ok there. dear, i would like to ask you to are happy to come with us for enjoying the party of birthday of harsh."
+# s = input("Enter the word for find it : ")
+
+# if s in text:
+#     print(f"{s} is present in the text")
+# else:
+#     print(f"{s} is not present in the text")
