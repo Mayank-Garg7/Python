bb88e2e0dbca1c2104b10274296d8fff3cd28c47
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2023-09-23 17:52:00 +0530

    Add files via upload

---
+++ ProblemsSolution/CloneTheList.py
@@ -0,0 +1,39 @@
+# How to clone or copy the list
+arr = [33,16,37,23,17,11,32,13]
+print(f"Original list : {arr}")
+
+# Approach 1 Using append method
+# new = []
+# for i in range(0, len(arr)):
+#     new.append(arr[i])
+# print(f"Cloned List : {new}")
+
+
+# Approach 2 Using slicing method
+# new = arr[ : ]
+# print(f"Cloned List : {new}")
+
+
+# Approach 3 Using exten method
+# new = []
+# new.extend(arr)
+# print(f"Cloned List : {new}")
+
+
+# Approach 4 Using " = " operator
+# new = arr
+# print(f"Cloned List : {new}")
+
+
+# Approach 5 Using list method
+# new = list(arr)
+# print(f"Cloned List : {new}")
+
+
+# Approach 6 Using copy method
+# print(f"Cloned List : {arr.copy()}")
+
+
+# Approach 7 Using list comprehension
+# new = [i for i in arr]
+# print(f"Cloned List : {new}")
