9c9b435625af913e28d1bfe82a838580d397177b
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2023-09-23 17:10:37 +0530

    Add some new files here

---
+++ BasicPython/stringfun.py
@@ -0,0 +1,11 @@
+story = "once upon the time there was a coder who's name is mayank write some code for finding some face detecting techniques"
+# print(story[:8])
+
+# String functions
+print(len(story))
+print(story.endswith("techniques"))
+print(story.count("on"))
+print(story.capitalize())
+print(story.find("name"))
+print(story.replace("mayank","hello dude and i'm a developer who get 1st rank in indian"))
+print()
