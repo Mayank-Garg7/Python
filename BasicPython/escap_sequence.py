05e3438e7d31a531b79b2542e968c8da52d9102b
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2023-09-23 17:04:21 +0530

    Add files via upload

---
+++ BasicPython/escap_sequence.py
@@ -0,0 +1,17 @@
+# st = "hello dude how are you there. I\n hope you \tare \\well and ok \'there."
+# print(st)
+
+# letter = '''Dear <|Name|>
+# You Are Selected.
+# Date: <|date|>   '''
+# a = input("Enter your name \n")
+# b = input("Enter todays date \n")
+# letter = letter.replace("<|Name|>", a)
+# letter = letter.replace("<|date|>", b) 
+# print("\n",letter)
+
+# text = "write a code for detecting double  space."
+
+# print(text.replace("  "," "))
+
+
