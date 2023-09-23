f172f8f0e1026b32a824345519866984cc7778c8
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2023-09-23 17:53:34 +0530

    Add files via upload

---
+++ ProblemsSolution/CountOccurance.py
@@ -0,0 +1,28 @@
+# Count the occurance of the element in the list
+
+arr = [3,5,2,7,9,7,1,7]
+print(f"List including all the elements : {arr}")
+a = int(input("Enter the value which is occured in more than one time : "))
+
+# Approach 1 Using for and if statement
+# count = 0
+# for i in range(0, len(arr)):
+#     if a == arr[i]:
+#         count = count+1
+# print(f"{a} is appear {count} times")
+
+
+# Approach 2 Using count method
+# count = arr.cnt(a)
+# print(f"{a} is appear {count} times")ount(a)
+# print(f"{a} is appear {count} times")
+
+
+# Approach 3 Using Counter method, in this method there is no need of input**
+# from collections import Counter
+# dic = Counter(arr)
+# print(f"Appearance of values are given here : {dic}")
+
+
+
+
---
+++ ProblemsSolution/fibonacci.py
@@ -0,0 +1,15 @@
+# Print the fibonacci series
+# series 0 1 1 2 3 5 8 13 21 and so on.....
+num = int(input("Enter the number : "))
+
+# Approach 1
+# a = 0
+# b = 1
+# print(f"{a}\n{b}")
+# for i in range(0,num):
+#     c = a+b
+#     a = b
+#     b = c
+#     print(c)
+
+# Approach 2
