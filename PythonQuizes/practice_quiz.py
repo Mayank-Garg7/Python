1b469550ac256e4a972e3621ada91c2880ef4f15
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2023-09-23 16:41:18 +0530

    Add files via upload

---
+++ PythonQuizes/practice_quiz.py
@@ -0,0 +1,37 @@
+# PRACTICE 1ST Make a function and call it
+# def greet(name):
+#     print("Good day "+name)
+# greet("mayank")
+
+
+# PRACTICE 2nd find the factorial by using function
+# def factorial(n):
+#     fact = 1
+#     for i in range(1, a+1):
+#         fact = fact*i
+#     return fact
+# a = int(input("Enter the number : "))
+# print(factorial(a))
+
+
+# PRACTICE 3rd find the factorial by using recursive function
+# def rec_fact(n):
+#     if n == 1 or n == 0:
+#         return 1
+#     return n * rec_fact(n-1)
+# a = int(input("Enter the number : "))
+# print(rec_fact(a))
+
+
+# PRACTICE 4th 0cel = 32farenheit 
+# def farenhite(cel):
+#     return (cel*(9/5))+32
+
+
+# PRACTICE 5th
+# PRACTICE 6th
+# PRACTICE 7th
+# PRACTICE 8th
+# PRACTICE 9th
+# PRACTICE 10th
+
