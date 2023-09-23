1a16fc300ea11d3480a0743d1e7e7031432bc90c
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2023-09-24 01:16:21 +0530

    Add files via upload

---
+++ ProblemsSolution/Secondlargest.py
@@ -0,0 +1,13 @@
+# How to find the second largest number in the list
+arr = [2,5,7,3,4,37,8,9,34]
+print(f"list : {arr}")
+
+# Approach 1
+# arr.sort()
+# print(f"Second largest number is : {arr[-2]}")
+
+
+
+# Approach 2 Using remove method
+# arr.remove(max(arr))
+# print(max(arr))
