983e2aa26cb4c97176eab64fe09c1cc2e0a4bb2f
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2023-09-24 01:19:16 +0530

    Add files via upload

---
+++ ProblemsSolution/swap.py
@@ -0,0 +1,19 @@
+# How to swap two numbers in python 
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
+print("Given values without Swapping are "+str(num1)+" and "+str(num2))
+# Approach 1
+# temp = num1
+# num1 = num2
+# num2 = temp
+# print(f"Given values After Swapping are "+str(num1)+" and "+str(num2))
+
+
+# Approach 2
+# num1,num2 = num2, num1
+# print(f"Given values After Swapping are "+str(num1)+" and "+str(num2))
+
+
+# Approach 3 hahaahaaa
+# One more smart way to do swapping without using any another variable
+# print(f"Given values After Swapping are "+str(num2)+" and "+str(num1))
