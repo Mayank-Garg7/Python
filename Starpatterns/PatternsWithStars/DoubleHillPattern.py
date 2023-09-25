5a0978a5d83373a835f3916877141a912cd27f75
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2023-09-25 23:08:28 +0530

    Add files via upload

---
+++ Starpatterns/PatternsWithStars/DoubleHillPattern.py
@@ -0,0 +1,26 @@
+# *****Double Hill/Pyramid pattern by star
+n = int(input("Enter the number of rows : "))
+
+
+
+# Approach 1
+# for i in range(n):
+#     for j in range(i,n):
+#         print("  ",end = "")
+#     for k in range(i):
+#         print("*",end=" ")
+#     for k in range(i+1):
+#         print("*",end=" ")
+#     for j in range(i,n-1):
+#         print(2*"  ",end = "")
+#     for k in range(i):
+#         print("*",end=" ")
+#     for k in range(i+1):
+#         print("*",end=" ")
+#     print("")
+
+
+
+
+
+
