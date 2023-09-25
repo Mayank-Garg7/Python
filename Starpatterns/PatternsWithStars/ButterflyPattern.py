3b0c341c12cce830b36de6076f372c70dce0d1bc
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2023-09-25 23:03:24 +0530

    Add files via upload

---
+++ Starpatterns/PatternsWithStars/ButterflyPattern.py
@@ -0,0 +1,25 @@
+# *****Butterfly pattern by star
+n = int(input("Enter the number of rows : "))
+
+
+
+# for i in range(n-1):
+#     for j in range(i+1):
+#         print("*",end = " ")
+#     for j in range(i,n-1):
+#         print(2*"  ",end = "")
+#     for k in range(i+1):
+#         print("*",end=" ")
+#     print("")
+# for i in range(n):
+#     for j in range(i,n):
+#         print("*",end = " ")
+#     for k in range(1,i+1):
+#         print(2*"  ",end="")
+#     for j in range(i,n):
+#         print("*",end =" ")
+#     print("")
+
+
+
+
