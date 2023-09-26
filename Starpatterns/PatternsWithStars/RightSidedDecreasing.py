60e93dfe3d511afad5ab946d2626de8aef76a966
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2023-09-26 23:52:09 +0530

    Add files via upload

---
+++ Starpatterns/PatternsWithStars/RightSidedDecreasing.py
@@ -0,0 +1,11 @@
+# *****Right Sided Decreasing Triangle by star
+n = int(input("Enter the number of rows : "))
+
+
+# Approach 1
+# for i in range(n):
+#     for k in range(i+1):
+#         print("  ",end="")
+#     for j in range(i,n):
+#         print("*",end =" ")
+#     print("")
