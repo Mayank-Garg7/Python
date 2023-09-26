762b74abc66c2ee6ccaf1698fcfa86f753927ec3
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2023-09-26 23:50:04 +0530

    Add files via upload

---
+++ Starpatterns/PatternsWithStars/LeftPascaltriangle.py
@@ -0,0 +1,13 @@
+# *****Left pascal's Triangle by star
+n = int(input("Enter the number of rows : "))
+
+
+
+# for i in range(n-1):
+#     for j in range(i+1):
+#         print("*",end = " ")
+#     print("")
+# for i in range(n):
+#     for j in range(i,n):
+#         print("*",end = " ")
+#     print("")
