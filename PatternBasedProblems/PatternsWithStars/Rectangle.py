3a1e781eb99e7c1cbc5efac82a810f95357ea720
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-05-25 22:27:41 +0530

    add pascal pyramid rectangle type star based problem solution

---
+++ PatternBasedProblems/PatternsWithStars/Rectangle.py
@@ -0,0 +1,16 @@
+# *****Rectangle by star
+n = int(input("Enter the number of rows : "))
+
+
+# Approach 1
+# for i in range(0, n):
+#     print(n*"* ")
+
+
+
+# Approach 2
+# for i in range(n):
+#     for j in range(n):
+#         print("*",end = " ")
+#     print("")
+
