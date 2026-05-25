32c4da0eb2bb91acca74977a487c6b9f06ad1415
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-05-25 22:29:01 +0530

    add side decreasing sandglass pattern type problem solutions

---
+++ PatternBasedProblems/PatternsWithStars/RightSidedIncreasing.py
@@ -0,0 +1,13 @@
+# *****Right Sided Increasing Triangle by star
+n = int(input("Enter the number of rows : "))
+
+# Approach 1
+# for i in range(n):
+#     for j in range(i,n):
+#         print(" ",end = " ")
+#     for k in range(i+1):
+#         print("*",end=" ")
+#     print("")
+
+
+# Approach 2
