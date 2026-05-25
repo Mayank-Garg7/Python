d7f11aa0f40723f5cb11ff4cac3bf8e2a4265a00
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-05-25 22:26:44 +0530

    add starr based Pattern problem solutions

---
+++ PatternBasedProblems/PatternsWithStars/DiamondPattern.py
@@ -0,0 +1,20 @@
+# *****Diamond pattern by star
+n = int(input("Enter the number of rows : "))
+
+
+# for i in range(n-1):
+#     for j in range(i,n):
+#         print("  ",end = "")
+#     for k in range(i):
+#         print("*",end=" ")
+#     for k in range(i+1):
+#         print("*",end=" ")
+#     print("")
+# for i in range(n):
+#     for k in range(i+1):
+#         print("  ",end="")
+#     for j in range(i+1,n):
+#         print("*",end =" ")
+#     for j in range(i,n):
+#         print("*",end =" ")
+#     print("")
---
+++ PatternBasedProblems/PatternsWithStars/IncreasingTriangle.py
@@ -0,0 +1,16 @@
+# Increasing triangle by stars
+n = int(input("Enter the number : "))
+
+
+
+# Approach 1
+# for i in range(n):
+#     print(i*" *")
+
+
+
+# Approach 2
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*",end = " ")
+#     print("")
