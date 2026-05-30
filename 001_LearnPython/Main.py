5ecffc9e24f8e543e3fbe8f1fbd911a2e58eb939
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-05-30 20:31:42 +0530

    add files

---
+++ 001_LearnPython/Main.py
@@ -0,0 +1,38 @@
+import os
+# print("hello world")
+# print(os.listdir())
+# a=7
+# b=9
+# print("yea i'm here ",a+b)
+# print("yea i'm here ",a/b)
+# print("yea i'm here ",a-b)
+# print("yea i'm here ",a*b)
+
+# a = 34
+# print(a)
+# a -=12
+# print(a)
+# a *=12
+# print(a)
+# a /=12
+# print(a)
+# a +=12
+# print(a)
+# print("hello dude")
+# a = input("Enter your name : ")
+# a = int(a)
+# print(type(a))
+
+# a=34
+# b=80
+# if(a>b):
+#     print("a is greater than b ")
+# else:
+#     print("b is greater than a ")
+
+a = int(input("Enter the number for finding the square and average"))
+b = int(input("Enter the number for finding the average"))
+c = (a+b)/2
+print("average value of entered numbers is ",c)
+print("square value of is : ",a*a)
+
