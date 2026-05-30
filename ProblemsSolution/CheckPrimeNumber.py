b5d059d358b33fe0ea1c6182cf3f76100b3e4897
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-05-30 22:08:04 +0530

    finally fix it

--- ProblemsSolution/CheckPrimeNumber.py
+++ ProblemsSolution/CheckPrimeNumber.py
@@ -1,5 +1,5 @@
 # Check the number is prime or not
-# Prime number is which has only 2 factors 1 and itself
+# Prime number is which has only 2 factors, 1 and itself
 # For example = 11 has only two factors 1 & 11(itself) it is prime no.
 # and 16 has many factors such as 1,2,4,8 & 16 so it is not a prime number
 num = int(input("Enter a number : "))
