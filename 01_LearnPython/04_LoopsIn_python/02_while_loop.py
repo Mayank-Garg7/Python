4bf84d098ffe4df95f15cf22dd4e49d7ae7f8940
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-06-10 11:58:05 +0530

    add some beginner level loops

---
+++ 01_LearnPython/04_LoopsIn_python/02_while_loop.py
@@ -0,0 +1,5 @@
+count = 0
+
+while count < 5:
+    print(count)
+    count += 1
---
+++ 01_LearnPython/04_LoopsIn_python/04_nested_while_loop.py
@@ -0,0 +1,10 @@
+i = 1
+
+while i <= 3:
+    j = 1
+
+    while j <= 3:
+        print(f"i={i}, j={j}")
+        j += 1
+
+    i += 1
